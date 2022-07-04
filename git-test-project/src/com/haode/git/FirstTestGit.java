e17cadddf8964e80e1d2e97b815c04614472847f
caofuchen <cfc508@163.com>
2022-07-04 22:02:00 +0800

    第一次提交到本地仓库

---
+++ git-test-project/src/com/haode/git/FirstTestGit.java
@@ -0,0 +1,9 @@
+package com.haode.git;
+
+public class FirstTestGit {
+
+    public static void main(String[] args) {
+        System.out.println("第一行：创建一个jar工程，测试使用git");
+        System.out.println("第二行：第一次就先两行吧！");
+    }
+}
