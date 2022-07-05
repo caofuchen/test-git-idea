ff751c9b21bc76c1f69f809ad39f1f36ae69c1a7
caofuchen <cfc508@163.com>
2022-07-05 19:14:25 +0800

    修改了FirstClass类

--- git-test-project/src/com/haode/git/FirstTestGit.java
+++ git-test-project/src/com/haode/git/FirstTestGit.java
@@ -5,8 +5,10 @@ public class FirstTestGit {
     public static void main(String[] args) {
         System.out.println("第一行：创建一个jar工程，测试使用git");
         System.out.println("第二行：第一次就先两行吧！");
+        //add a 注释
     }
     public void addMethod(){
         System.out.println("第一次提交后，新添加了一个方法");
+        System.out.println("第二次修改后，又添加了一个输出");
     }
 }
